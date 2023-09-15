6ba3fb8d188ca4f00e4bcab1c4144f6ac82d8e80
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-09-15 12:39:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,46 +55,46 @@ source_img = None
 #if source_radio == settings.IMAGE:
 source_img = st.sidebar.file_uploader("Choose an image...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
 
-    col1, col2 = st.columns(2)
+col1, col2 = st.columns(2)
 
-    with col1:
-        try:
-            if source_img is None:
-                default_image_path = str(settings.DEFAULT_IMAGE)
-                default_image = PIL.Image.open(default_image_path)
-                st.image(default_image_path, caption="Default Image",
-                         use_column_width=True)
-            else:
-                uploaded_image = PIL.Image.open(source_img)
-                st.image(source_img, caption="Uploaded Image",
-                         use_column_width=True)
-        except Exception as ex:
-            st.error("Error occurred while opening the image.")
-            st.error(ex)
-
-    with col2:
+with col1:
+    try:
         if source_img is None:
-            default_detected_image_path = str(settings.DEFAULT_DETECT_IMAGE)
-            default_detected_image = PIL.Image.open(
-                default_detected_image_path)
-            st.image(default_detected_image_path, caption='Detected Image',
+            default_image_path = str(settings.DEFAULT_IMAGE)
+            default_image = PIL.Image.open(default_image_path)
+            st.image(default_image_path, caption="Default Image",
                      use_column_width=True)
         else:
-            if st.sidebar.button('Detect Damage'):
-                res = model.predict(uploaded_image,
-                                    conf=confidence
-                                    )
-                boxes = res[0].boxes
-                res_plotted = res[0].plot()[:, :, ::-1]
-                st.image(res_plotted, caption='Detected Image',
-                         use_column_width=True)
-                try:
-                    with st.expander("Detection Results"):
-                        for box in boxes:
-                            st.write(box.data)
-                except Exception as ex:
-                    # st.write(ex)
-                    st.write("No image is uploaded yet!")
+            uploaded_image = PIL.Image.open(source_img)
+            st.image(source_img, caption="Uploaded Image",
+                     use_column_width=True)
+    except Exception as ex:
+        st.error("Error occurred while opening the image.")
+        st.error(ex)
+
+with col2:
+    if source_img is None:
+        default_detected_image_path = str(settings.DEFAULT_DETECT_IMAGE)
+        default_detected_image = PIL.Image.open(
+            default_detected_image_path)
+        st.image(default_detected_image_path, caption='Detected Image',
+                 use_column_width=True)
+    else:
+        if st.sidebar.button('Detect Damage'):
+            res = model.predict(uploaded_image,
+                                conf=confidence
+                                )
+            boxes = res[0].boxes
+            res_plotted = res[0].plot()[:, :, ::-1]
+            st.image(res_plotted, caption='Detected Image',
+                     use_column_width=True)
+            try:
+                with st.expander("Detection Results"):
+                    for box in boxes:
+                        st.write(box.data)
+            except Exception as ex:
+                # st.write(ex)
+                st.write("No image is uploaded yet!")
 
 # elif source_radio == settings.VIDEO:
 #     helper.play_stored_video(confidence, model)
