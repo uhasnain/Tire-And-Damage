376fe115d87ded48c68bfe37e7406480b50b147a
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-09-15 14:10:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,15 +18,25 @@ st.set_page_config(
 )
 
 # Main page heading
-st.title("Tire Defect and Damage Segmentation")
+st.title("Tire Defect and Damage Detection")
 
 #Logo
-st.sidebar.image("logo-normal.png")
+#st.sidebar.image("logo-normal.png")
 
 
 # Sidebar
 st.sidebar.header("Model Configurations")
-  
+# Model Options
+#model_type = st.sidebar.radio(
+    #"Task Type:", ['Segmentation'])
+
+confidence = float(st.sidebar.slider(
+    "Select Model Confidence", 25, 100, 40)) / 100
+
+# # Selecting Detection Or Segmentation
+# if model_type == 'Detection':
+#     model_path = Path(settings.DETECTION_MODEL)
+#if model_type == 'Segmentation':
 model_path = Path(settings.SEGMENTATION_MODEL)
 
 # Load Pre-trained ML Model
@@ -53,10 +63,12 @@ if source_radio == settings.IMAGE:
             if source_img is None:
                 default_image_path = str(settings.DEFAULT_IMAGE)
                 default_image = PIL.Image.open(default_image_path)
-                st.image(default_image_path, caption="Default Image", use_column_width=True)
+                st.image(default_image_path, caption="Default Image",
+                         use_column_width=True)
             else:
                 uploaded_image = PIL.Image.open(source_img)
-                st.image(source_img, caption="Uploaded Image", use_column_width=True)
+                st.image(source_img, caption="Uploaded Image",
+                         use_column_width=True)
         except Exception as ex:
             st.error("Error occurred while opening the image.")
             st.error(ex)
@@ -64,32 +76,32 @@ if source_radio == settings.IMAGE:
     with col2:
         if source_img is None:
             default_detected_image_path = str(settings.DEFAULT_DETECT_IMAGE)
-            default_detected_image = PIL.Image.open(default_detected_image_path)
-            st.image(default_detected_image_path, caption='Detected Image',use_column_width=True)
-            
+            default_detected_image = PIL.Image.open(
+                default_detected_image_path)
+            st.image(default_detected_image_path, caption='Detected Image',
+                     use_column_width=True)
         else:
             if st.sidebar.button('Detect Damage'):
-                st.sidebar.success("Accuracy : 99%")
-                res = model.predict(uploaded_image, conf=confidence)
+                res = model.predict(uploaded_image,
+                                    conf=confidence
+                                    )
                 boxes = res[0].boxes
                 res_plotted = res[0].plot()[:, :, ::-1]
-                st.image(res_plotted, caption='Detected Image',use_column_width=True)
-            
+                st.image(res_plotted, caption='Detected Image',
+                         use_column_width=True)
                 try:
                     with st.expander("Detection Results"):
                         for box in boxes:
-                            
                             st.write(box.data)
-                             
                 except Exception as ex:
-                    st.write(ex)
+                    # st.write(ex)
                     st.write("No image is uploaded yet!")
 
 # elif source_radio == settings.VIDEO:
 #     helper.play_stored_video(confidence, model)
 
-elif source_radio == settings.WEBCAM:
-    helper.play_webcam(confidence, model)
+# elif source_radio == settings.WEBCAM:
+#     helper.play_webcam(confidence, model)
 
 # elif source_radio == settings.RTSP:
 #     helper.play_rtsp_stream(confidence, model)
@@ -99,4 +111,3 @@ elif source_radio == settings.WEBCAM:
 
 else:
     st.error("Please select a valid source type!")
-
