9124edfd7e12cda3a249b1359a5a8a1e0a6b4261
uhasnain <101257089+uhasnain@users.noreply.github.com>
2024-06-07 00:14:21 +0500

    settingsvideo.py

--- settingsvideo.py
+++ settingsvideo.py
@@ -29,7 +29,7 @@ DEFAULT_IMAGE = IMAGES_DIR / 'Copy of DJI_0667.JPG'
 DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'Untitled.png'
 
 # Videos config
-VIDEO_DIR = ROOT / 'videos'
+VIDEO_DIR = ROOT / 'images'
 VIDEO_1_PATH = VIDEO_DIR / 'video_1.mp4'
 VIDEO_2_PATH = VIDEO_DIR / 'video_2.mp4'
 VIDEO_3_PATH = VIDEO_DIR / 'video_3.mp4'
